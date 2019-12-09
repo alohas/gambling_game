bfb95b75cc082f14ffb94a62376c43c7dbdc0e7c
Ewa Utracka <ewa.utr@gmail.com>
2019-12-09 11:54:50 +0100

    menu and user shows on click

--- script/script.js
+++ script/script.js
@@ -1,18 +1,42 @@
 "use strict";
 
 const moreBtn = document.querySelector(".slider_right-button");
-let slided = false;
+const userBtn = document.querySelector(".nav_user");
+const menuBtn = document.querySelector(".nav_menu");
+let slidedMore = false;
+let slidedMenu = false;
+let slidedUser = false;
 
 moreBtn.addEventListener("click", e => {
-  if (!slided) {
+  if (!slidedMore) {
     document.querySelector(".slider").style.animation = "";
     document.querySelector(".slider").style.animation =
       "sliderFullIn 0.4s ease-in forwards";
-    slided = true;
+    slidedMore = true;
   } else {
     document.querySelector(".slider").style.animation = "";
     document.querySelector(".slider").style.animation =
       "sliderOut 0.4s ease-in forwards";
-    slided = false;
+    slidedMore = false;
+  }
+});
+
+userBtn.addEventListener("click", e => {
+  if (!slidedUser) {
+    document.querySelector(".login").style.visibility = "visible";
+    slidedUser = true;
+  } else {
+    document.querySelector(".login").style.visibility = "hidden";
+    slidedUser = false;
+  }
+});
+
+menuBtn.addEventListener("click", e => {
+  if (!slidedMenu) {
+    document.querySelector(".menu").style.visibility = "visible";
+    slidedMenu = true;
+  } else {
+    document.querySelector(".menu").style.visibility = "hidden";
+    slidedMenu = false;
   }
 });
